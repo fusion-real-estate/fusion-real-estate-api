36a30fdc742e8d54178d7262cdb45f978f2deb6d
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-29 09:53:31 -0300

    :tada: first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '37093196bd4dc044dcf672b43a70cc83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '284fecbe-47e7-4fc5-a061-80f1560b62cf'
+};
